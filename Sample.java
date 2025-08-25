beb077f24867da261fcd6f263fe917b8b7729323
sakshi sirgan <sakshisirgan03@gmail.com>
2025-08-25 12:40:20 +0530

    Updated the code

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + Git!");
+        System.out.println("Hello from GitJenkins!");
+	System.out.println("Edited");
     }
 }
